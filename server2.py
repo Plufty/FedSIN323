d278eea8380b99fd74ad5a60b8f8553373ef03ab
Gleidson Barbosa <gleidson.barbosa@fattocs.com.br>
2023-11-09 09:25:30 -0300

    Change Server path

--- server2.py
+++ server2.py
@@ -4,8 +4,6 @@ import flwr as fl
 from flwr.common import Metrics
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-import datetime
 
 # Define metric aggregation function
 def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
@@ -38,22 +36,19 @@ strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=weighted_av
 # Inicializar lista para armazenar métricas
 all_metrics = []
 
-# Função de callback para coletar métricas após cada rodada de treinamento
-def callback(metrics: List[Tuple[int, Metrics]]) -> None:
-    # Adicionar métricas à lista
-    all_metrics.extend(metrics)
-
-# Start Flower server
+# Iniciar servidor Flower com método fit
 fl.server.start_server(
     server_address="0.0.0.0:8080",
     config=fl.server.ServerConfig(num_rounds=3),
     strategy=strategy,
-    on_fit_round_end=callback,  # Adiciona o callback
 )
 
-# Após o término do treinamento, chamar a função para plotar e salvar métricas
+# Realizar rodadas de treinamento
+for round_num in range(1, 4):  # Número de rodadas (ajuste conforme necessário)
+    metrics = fl.server.server.fit(round_num=round_num)
+    all_metrics.extend(metrics)
 
-date_now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-output_path = "./outputs/FED.pdf"
-os.makedirs(output_path, exist_ok=True)
+# Após o término do treinamento, chamar a função para plotar e salvar métricas
+output_path = "./grafico.pdf"
 plot_and_save_metrics(all_metrics, output_path)
+
